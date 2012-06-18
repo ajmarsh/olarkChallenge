463cc69a4dbefc477b383fae5cf5e66b530b09b3
ajmarsh <ajmarsh@gmail.com>
2012-06-18 13:04:07 -0400

    put loops into 010-provision script

--- bin/010-provisionServers.sh
+++ bin/010-provisionServers.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-# This script will provision 5 servers at Amazon EC2 and assign IP addresses to them
+# This script will provision 5 servers at Amazon EC2 and assign IP addresses
+# to them
 # ajmarsh@gmail.com 6/16/2012
 
-# set up some variables
+# variable block (think about seperate environment file if this gets much longer)
 EC2_PRIVATE_KEY=/Users/ajmarsh/olarkSecret/pk-WGOZWJT7ZQ6D2GPF5WFEMI5EMZYBYMYE.pem
 EC2_CERT=/Users/ajmarsh/olarkSecret/cert-WGOZWJT7ZQ6D2GPF5WFEMI5EMZYBYMYE.pem
+# Number of servers to deploy
+n_web=2
+n_proxy=1
+n_load=2
+n_total=$(( $n_web + $n_proxy + $n_load ))
 
-# rewrite as loop in case you want to expand the numebr of servers
+
 # obtain static IP address for our instances
+echo "obtaining your IP addresses, this will take a moment..."
 
-IPADDR0=$(/Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
-IPADDR1=$(/Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
-IPADDR2=$(/Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
-IPADDR3=$(/Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
-IPADDR4=$(/Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
-#
-echo " your IP addresses : $IPADDR1 $IPADDR2 $IPADDR3 $IPADDR4 $IPADDR5"
+for i in `seq 0 $n_total`; do
+    IPADDR[$i]=$( /Users/ajmarsh/ec2-api-tools/bin/ec2-allocate-address | awk '{ print $2; }' )
+done
+
+echo " your IP addresses : "
+
+for i in `seq 0 $n_total`; do
+	echo "${IPADDR[$i]} "
+done
 
 # generate keypair if it does not exist
+
 if [ -f ~ajmarsh/olarkSecret/olarkKey.pem ];
 then
 	echo "Using existing keys"
@@ -28,34 +38,30 @@ else
 	chmod 600  ~ajmarsh/olarkSecret/olarkKey.pem
 fi
 
-# launch 2 webservers 
-# rewrite as loop in case you want to chage number of servers
-ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey -g olarkSec --instance-type t1.micro -z us-east-1a
-ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey -g olarkSec --instance-type t1.micro -z us-east-1a
+# launch N webservers
+for i in `seq 1 $n_web`; do
+	ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey \
+		-g olarkSec --instance-type t1.micro -z us-east-1a
+done
 
-# launch 1 haproxy server
-# rewrite as loop in case you want to chage number of servers
-ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey -g olarkSec --instance-type t1.micro -z us-east-1a
+# launch N haproxy server
+for i in `seq 1 $n_proxy`; do
+	ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey \
+		-g olarkSec --instance-type t1.micro -z us-east-1a
+done
 
-# launch 2 siege util servers
-# rewrite as loop in case you want to chage number of servers
-ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey -g olarkSec --instance-type t1.micro -z us-east-1a
-ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey -g olarkSec --instance-type t1.micro -z us-east-1a
+# launch N siege load test servers
+for i in `seq 1 $n_load`; do
+	ec2-run-instances ami-a29943cb -K $EC2_PRIVATE_KEY -C $EC2_CERT -k olarkKey \
+		-g olarkSec --instance-type t1.micro -z us-east-1a
+done
 
 # assign static IP addresses to our instances
-array=( $( ec2-describe-instances | grep -v terminated | awk '{ print $2; }' | grep "i-" ) )
-SERVER0=${array[0]}
-SERVER1=${array[1]}
-SERVER2=${array[2]}
-SERVER3=${array[3]}
-SERVER4=${array[4]}
+SERVER=( $( ec2-describe-instances | grep -v terminated | awk '{ print $2; }' | grep "i-" ) )
 
-# not sure what my probelm is with loops today
-ec2-associate-address -i $SERVER0 $IPADDR0
-ec2-associate-address -i $SERVER1 $IPADDR1
-ec2-associate-address -i $SERVER2 $IPADDR2
-ec2-associate-address -i $SERVER3 $IPADDR3
-ec2-associate-address -i $SERVER4 $IPADDR4
+for i in `seq 0 $n_total` ; do
+	ec2-associate-address -i ${SERVER[$i]} ${IPADDR[$i]}
+done
 
 ec2-describe-addresses
 
