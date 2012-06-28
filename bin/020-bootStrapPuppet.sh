87357da2b13919a8953c13ae2908f31df86f7f0c
ajmarsh <ajmarsh@gmail.com>
2012-06-27 20:54:34 -0400

    update to 020 deployment script

--- bin/020-bootStrapPuppet.sh
+++ bin/020-bootStrapPuppet.sh
@@ -3,23 +3,11 @@
 # assign static IP addresses to our instances
 IPADDR=( $( ec2-describe-addresses | awk '{ print $2; }' | grep -v 174.129.2.252 ) )
 
-echo ${IPADDR[0]}
-echo ${IPADDR[1]}
-echo ${IPADDR[2]}
-
-
 for i in `seq 0 ${#IPADDR[@]}`; do
 	echo "installing puppet on ${IPADDR[$i]} "
 	ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@${IPADDR[$i]} "sudo apt-get -y install puppet"
 	ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@${IPADDR[$i]} 'sudo echo "174.129.2.252 	ec2-174-129-2-252.compute-1.amazonaws.com 	puppet" >> /etc/hosts '
 done
 
-
-# clean up, put in loop
-#ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@$IPADDR0 "sudo apt-get -y install puppet"
-#ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@$IPADDR1 "sudo apt-get -y install puppet"
-#ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@$IPADDR3 "sudo apt-get -y install puppet"
-#ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@$IPADDR4 "sudo apt-get -y install puppet"
-#ssh -i ~ajmarsh/olarkSecret/olarkKey.pem ubuntu@$IPADDR5 "sudo apt-get -y install puppet"
 
 
